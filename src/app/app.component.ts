c07eb8637ce1f2e3a34885da11ccea013a66ab72
NormanA1una <baciloferoz@gmail.com>
2023-04-26 16:39:14 -0600

    Fetchin data...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./service/courses.service";
 
 @Component({
   selector: "app-root",
@@ -22,11 +23,15 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
 
-  constructor(private http: HttpClient) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+    this.courses$ = this.coursesService.loadCourses();
+  }
 
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params });
+  save(course: Course) {
+    this.coursesService
+      .saveCourse(course)
+      .subscribe(() => console.log("Course saved!"));
   }
 }
